6472032095c257df25985bae5e99ac2f31ac1b5a
Vito <topvetal@gmail.com>
2021-01-14 19:59:08 +0200

    last_comm

--- app.js
+++ app.js
@@ -57,4 +57,22 @@ Vue.createApp({
     },
     items: [1, 2, 3, 4, 5, 6]
   }),
+  methods: {
+    addItem(event) {
+      this.items.unshift(this.$refs.myInput.value)
+      this.$refs.myInput.value = ''
+      console.log(event.key)
+    },
+    remove(i) {
+      this.items.splice(i, 1)
+    },
+    log(item) {
+      console.log('item', item)
+    },
+  },
+  computed: {
+    eventItems() {
+      return this.items.filter(i => i % 2 === 0)
+    },
+  }
 }).mount('#app')
